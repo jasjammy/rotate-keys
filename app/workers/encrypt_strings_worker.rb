66b4386592fa0976d38ae9bb970c3e63779a2448
jasjammy <joanne.colaco@gmail.com>
2019-01-21 10:48:25 -0500

    refactor perfrom function for encrypt_strings_worker

--- app/workers/encrypt_strings_worker.rb
+++ app/workers/encrypt_strings_worker.rb
@@ -1,14 +1,20 @@
 class EncryptStringsWorker
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
-  # sidekiq_options retry: false
+  sidekiq_options :queue => :background
 
   def perform
-    # reencrypt all records in the database
+    # find the primary key and make it false
+    if !DataEncryptingKey.primary.nil?
+      DataEncryptingKey.primary.update_attributes!(primary: false)
+    end
 
-    EncryptedString.in_batches.each do |record|
-      record.set_data_encrypting_key
-      record.save
+    # generate a new key
+    new_key = DataEncryptingKey.generate!(primary: true)
+
+    # reencrypt all records in the database
+    EncryptedString.all.in_batches.each do |record|
+      record.first.reencrypt!(new_key)
     end
   end
 end
