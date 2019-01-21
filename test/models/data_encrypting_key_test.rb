d3457367d2297eaa9d9b68f989499e7b0f1f32e4
jasjammy <joanne.colaco@gmail.com>
2019-01-20 20:41:11 -0500

    add model tests

--- test/models/data_encrypting_key_test.rb
+++ test/models/data_encrypting_key_test.rb
@@ -1,14 +1,26 @@
 require 'test_helper'
 
 class DataEncryptingKeyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
   test ".generate!" do
     assert_difference "DataEncryptingKey.count" do
       key = DataEncryptingKey.generate!
       assert key
     end
+  end
+
+  test "#create fails when key is blank" do
+    new_key = DataEncryptingKey.create(primary: true)
+    new_key.save
+    assert_not new_key.save
+  end
+
+  test "#create succeeds when attributes are valid" do
+    new_key = DataEncryptingKey.
+        create(
+            key: "thisisakey",
+            primary: true
+        )
+    assert new_key.save
   end
 end
--- test/models/encrypted_string_test.rb
+++ test/models/encrypted_string_test.rb
@@ -2,7 +2,41 @@ require 'test_helper'
 
 class EncryptedStringTest < ActiveSupport::TestCase
 
-  test 'Missing tests! Please fix' do
+  test '#create succeeds with valid attributes and primary key present' do
+    primary_key = DataEncryptingKey.generate!(primary: true)
+    es = EncryptedString.
+        create(
+            value: "decrypted string",
+            data_encrypting_key_id:primary_key.id
+        )
+    assert es.save
+  end
 
+  test '#create does not succeed when there is no primary Data Encrypting Key' do
+    assert_raises {
+      EncryptedString.
+        create(
+            value: "decrypted string"
+        )
+    }
+  end
+
+  test "#update reencrypts the value with new primary key" do
+    primary_key = DataEncryptingKey.generate!(primary: true)
+    es = EncryptedString.
+        create(
+            value: "decrypted string",
+            data_encrypting_key_id:primary_key.id
+        )
+    primary_key.primary = false
+    primary_key.save
+    encrypted_value = es.encrypted_value
+    another_primary_key = DataEncryptingKey.generate!(primary: true)
+    another_primary_key.save
+    assert_not_same another_primary_key.key, es.encryption_key
+
+    assert es.rencrypt!(another_primary_key)
+    assert_equal another_primary_key.key, es.encryption_key
+    assert_not_equal encrypted_value, es.encrypted_value
   end
 end
