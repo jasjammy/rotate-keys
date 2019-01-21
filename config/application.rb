6f21f3eec62a6c45a4f3c3fafdc390802cada694
jasjammy <joanne.colaco@gmail.com>
2019-01-21 10:47:54 -0500

    Autoload Sidekiq workers in application.rb file

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module InterviewProject
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_job.queue_adapter = :sidekiq
+    config.autoload_paths += %W(#{config.root}/app/workers)
+    config.eager_load_paths += %W(#{config.root}/app/workers)
   end
 end
