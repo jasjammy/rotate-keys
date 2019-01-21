b9d7cc9a0fcfe3ba18f3483d151f846eb703e4c6
jasjammy <joanne.colaco@gmail.com>
2019-01-20 20:48:30 -0500

    Add sidekiq to test/test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "sidekiq/testing"
+require "sidekiq-status"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
